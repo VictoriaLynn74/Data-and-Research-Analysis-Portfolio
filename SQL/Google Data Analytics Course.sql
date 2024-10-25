150692df3c24cd03d07a986361f47e2ba58fc2eb
Victoria Wairimu <39093685+VictoriaLynn74@users.noreply.github.com>
2024-10-25 12:53:03 +0300

    Update Google Data Analytics Course.sql

--- SQL/Google Data Analytics Course.sql
+++ SQL/Google Data Analytics Course.sql
@@ -383,6 +383,73 @@ GROUP BY
   warehouse_name
 HAVING
   COUNT(Orders.order_id) > 0
+
+--CALCULATIONS IN SQL
+--using uploaded dataset from Kaggle
+
+SELECT 
+	Date,
+	Region,
+	Small_Bags,
+	Large_Bags,
+	XLarge_Bags,
+	Total_Bags,
+	Small_Bags + Large_Bags + XLarge_Bags AS Total_Bags_Calc
+
+ FROM `striking-gadget-215217.avocado_data.avocado_prices` 
+--Percentages
+SELECT 
+	Date,
+	Region,
+	Total_Bags,
+	Small_Bags,
+	(Small_Bags / Total_Bags)*100 AS Small_Bags_Percent
+
+ FROM `striking-gadget-215217.avocado_data.avocado_prices` 
+ 
+ WHERE Total_Bags <> 0
+
+--citibike public dataset
+ SELECT 
+EXTRACT (YEAR FROM starttime) AS year,
+COUNT(*) AS number_of_rides
+
+ FROM `bigquery-public-data.new_york_citibike.citibike_trips` 
+ GROUP BY year
+ ORDER BY year DESC
+
+--new_york_subway public dataset
+SELECT 
+
+station_name,
+ridership_2013,
+ridership_2014,
+ridership_2014-ridership_2013 AS change_2014_raw
+FROM `bigquery-public-data.new_york_subway.subway_ridership_2013_present` 
+--Average Calculation 
+SELECT 
+station_name,
+ridership_2013,
+ridership_2014,
+ridership_2015,
+ridership_2016,
+(ridership_2013+ridership_2014+ridership_2015+ridership_2016)/4 AS Average
+
+ FROM `bigquery-public-data.new_york_subway.subway_ridership_2013_present` 
+--Using Temporary Table WITH clause
+
+
+WITH trips_over_1_hour AS (
+SELECT *
+ FROM `bigquery-public-data.new_york_citibike.citibike_trips` 
+
+ WHERE tripduration>=3600
+)
+## Count how many trips are over 60 minutes
+
+SELECT 
+COUNT(*) AS cnt
+FROM trips_over_1_hour
  
 
 
