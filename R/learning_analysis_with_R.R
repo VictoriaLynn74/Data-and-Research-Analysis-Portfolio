d7aac8d508af68449b379c072397c5e6c0c57cd1
Victoria Wairimu <39093685+VictoriaLynn74@users.noreply.github.com>
2024-11-12 10:49:43 +0300

    Update learning_analysis_with_R.R

--- R/learning_analysis_with_R.R
+++ R/learning_analysis_with_R.R
@@ -1,33 +1,55 @@
-#installing package
-
- install.packages("palmerpenguins")
+# installing package
+install.packages("palmerpenguins")
 library("palmerpenguins")
 summary(penguins)
 View(penguins)
 
-#installing Tidyverse
+# installing Tidyverse
 install.packages("tidyverse")
 library(tidyverse)
-#creating vizualization
+
+# creating vizualization
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+geom_point(aes(colour = species))
 
-#Creating a variable
+# Creating a variable
 var1 <- 1999
 
-#creating a vector
+# creating a vector
 vec1 <- c(13.5, 12.2, 54.7)
 
-#creating a matrix
+# creating a matrix
 matrix(c(3:8), nrow = 2)
 matrix(c(3:8), ncol = 2)
 
-#creating data frames 
+# creating data frames 
 data.frame(x = c(1, 2, 3) , y = c(1.5, 5.5, 7.5))
 
-#Performing Calculations
+# Performing Calculations
 quarter_1_sales <- 35657.98
 quarter_2_sales <- 43810.55
 midyear_sales <- quarter_1_sales + quarter_2_sales
+
+# Viewing Data - Diamonds Tidyverse
+head(diamonds)
+
+# summarizing data 
+glimpse(diamonds)
+
+# cleaning data
+# 1.Renaming
+rename(diamonds, carat_new = carat)
+# Summary statistics
+summarize(diamonds, mean_carat = mean(carat))
+
+# Vizualization
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +
+  geom_point()
+
+# To seperate components in a visual 
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +
+  geom_point() +
+  facet_wrap(~cut)
+
 
 
 
