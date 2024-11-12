1159bc35f62c22f24817c69d8855fec401f94eca
Victoria Wairimu <39093685+VictoriaLynn74@users.noreply.github.com>
2024-11-12 08:55:03 +0300

    Update analysiswithr.R

--- R/analysiswithr.R
+++ R/analysiswithr.R
@@ -1,1 +1,10 @@
+#installng package
+ install.packages("palmerpenguins")
+library("palmerpenguins")
+summary(penguins)
+View(penguins)
+install.packages("tidyverse")
+library(tidyverse)
+#creating vizualization
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+geom_point(aes(colour = species))
 
