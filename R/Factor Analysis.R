db36de8f631b43bdd6e5ccf4a6af5e455d351b37
Victoria Wairimu <39093685+VictoriaLynn74@users.noreply.github.com>
2025-01-09 20:47:01 +0300

    Update Factor Analysis.R

--- R/Factor Analysis.R
+++ R/Factor Analysis.R
@@ -17,3 +17,12 @@ bartlett.test(leadership_model)
 orgperfom_model = data.frame(Deliveryontime, Image, Newproducts, Processes, Newcustomer, Retention, Swiftresponse, Clearlydefined)
 KMO(orgperfom_model)
 bartlett.test(orgperfom_model)     
+
+# Factor Analysis
+
+coord_fa <- fa(coord_model, nfactors = 2, rotate = "varimax") # Adjust nfactors based on your needs
+print(coord_fa)
+leadership_fa <- fa(leadership_model, nfactors = 2, rotate = "varimax")
+print(leadership_fa)
+orgperfom_fa <- fa(orgperfom_model, nfactors = 2, rotate = "varimax")
+print(orgperfom_fa)
