e06dc6b563430e3a3f945d2e145656f0a9fcff24
Victoria Wairimu <39093685+VictoriaLynn74@users.noreply.github.com>
2025-01-09 20:41:05 +0300

    Update learning_analysis_with_R.R

--- R/learning_analysis_with_R.R
+++ R/learning_analysis_with_R.R
@@ -232,25 +232,7 @@ ggplot(data=penguins)+
 ggsave("Palmer Penguins.png")
                                                                                                                           
                                                                                                                           
- # Factor Analysis - KMO and Bartlett's Test
-                                                                                                                          
-install.packages("haven")
-install.packages("psych")
-library(psych)
-                                                                                                                          
-attach(updated_organizational_data)
-                                                                                                                          
-coord_model= data.frame(Operationcord, Collaborates, Efectivchange, Strategicpart, Reviewbuss, Effectivecod, Leadership )
-KMO(coord_model)
-bartlett.test(coord_model)
-                                                                                                                          
-leadership_model = data.frame(Settinggoals, Tolerance, Continousplng, Communicatn, Inyouropinion, Decisionmakn, Stronglearning)
-KMO(leadership_model)
-bartlett.test(leadership_model)
-                                                                                                                          
-orgperfom_model = data.frame(Deliveryontime, Image, Newproducts, Processes, Newcustomer, Retention, Swiftresponse, Clearlydefined)
-KMO(orgperfom_model)
-bartlett.test(orgperfom_model)                                                                                                                          
+                                                                                                                         
                                                                                                                           
                                                                                                                           
 
